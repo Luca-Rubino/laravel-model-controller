2f378e4cbfc7ef21f0d591544dd02d7e79b043ff
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-18 09:39:39 +0200

    Inserisco dati del database in pagina

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -4,9 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite('resources/scss/app.scss', 'resources/js/app.js')
-    <title>laravel-comics</title>
+    <title>@yield('page-title', 'Home')</title>
 </head>
 <body>
-    <h1>Hello World</h1>
+    <h1>movie list</h1>
+    <section>
+    @foreach($movies as $movie)
+        <article>
+            <p>{{ $movie['title']}}</p>
+            <p>{{ $movie['original_title']}}</p>
+            <p>{{ $movie['nationality']}}</p>
+            <p>{{ $movie['date']}}</p>
+            <p>{{ $movie['vote']}}</p>
+        </article>    
+    @endforeach
+    </section>
 </body>
 </html>
